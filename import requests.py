8a8492357bc869c40a57c7fc78e5a503d08d0397
Kumail Gulamhusein <61838026+KumailGulamhusein@users.noreply.github.com>
2026-02-02 12:18:57 +0300

    Delete import requests.py

--- import requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-    'User-Agent': '[DISCORD] on Discord',
-    'From': '[EMAIL]'
-}
-response = requests.get(f"https://prices.runescape.wiki/api/v1/osrs/latest?id={2}", headers=headers)
-data = response.json()
-print(data)
